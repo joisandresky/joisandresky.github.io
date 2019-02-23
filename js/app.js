d4cd887cfd7aa786328c85c027cfd7d0fa6c8bd1
Jois Andresky <dat7joee@gmail.com>
2019-02-23 13:09:31 +0700

    penambahan console log

--- js/app.js
+++ js/app.js
@@ -1,2 +1,10 @@
 console.log('HELLO WORLD');
 console.log('HELLOW BOOTCAMP');
+console.log('HELLOW BOOTCAMP');
+console.log('HELLOW BOOTCAMP');
+console.log('HELLOW BOOTCAMP');
+console.log('HELLOW BOOTCAMP');
+console.log('HELLOW BOOTCAMP');
+console.log('HELLOW BOOTCAMP');
+console.log('HELLOW BOOTCAMP');
+console.log('HELLOW BOOTCAMP');
